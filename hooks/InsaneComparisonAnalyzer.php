e3ac706e725df2dc4ddb20ea0946d6e8168ca7a7
orklah <ronan_morin@hotmail.com>
2020-12-07 18:35:05 +0100

    fix bug

--- hooks/InsaneComparisonAnalyzer.php
+++ hooks/InsaneComparisonAnalyzer.php
@@ -105,7 +105,7 @@ class InsaneComparisonAnalyzer implements AfterExpressionAnalysisInterface
         $eligible_string = null;
         foreach ($string_operand->getAtomicTypes() as $possibly_string) {
             if ($possibly_string instanceof TLiteralString) {
-                if(!preg_match('#\D#', $possibly_string->value[0] ?? '')) {
+                if(preg_match('#\D#', $possibly_string->value[0] ?? '')) {
                     $eligible_string = $possibly_string;
                     break;
                 }
